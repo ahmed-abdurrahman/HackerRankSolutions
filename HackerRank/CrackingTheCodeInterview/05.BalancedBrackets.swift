a839779b6c82fd4ccf7a64443abeb10a0e4e8726
A. Abdurrahman <ahmed.abdurrahman@begether.net>
2017-04-02 17:34:11 +0200

    @Balanced Brackets: Optimized passing submissiong

--- HackerRank/CrackingTheCodeInterview/05.BalancedBrackets.swift
+++ HackerRank/CrackingTheCodeInterview/05.BalancedBrackets.swift
@@ -27,41 +27,38 @@ class BalancedBrackets {
     func isBalanced(inputArray: [String]) -> [Bool] {
         
         var result = [Bool]()
-        var stack :[Character]!
+        
         
         for input in inputArray {
             
-            stack = [Character]()
-            
-            checkLoop: for (i,char) in input.characters.enumerated() {
-                
-                switch char {
-                case "(","{","[":
-                    stack.append(char)
-                case ")","}","]":
-                    
-                    if let top = stack.popLast()
-                        , counterBracket[char]! == top {
-                        // It's Ok, continue checkig..
-                    } else {
-                        result.append(false)
-                        break checkLoop
-                    }
-                    
-                default:
-                    result.append(false)
-                    break checkLoop
-                }
-                
-                if i == input.characters.count - 1 {
-                    result.append(stack.isEmpty)
-                }
-            }
-            
-            
+            result.append(isBalanced(input: input))
         }
         
         return result
+    }
+    
+    func isBalanced(input: String) -> Bool {
+        var stack = [Character]()
+        
+        checkLoop: for (_,char) in input.characters.enumerated() {
+            
+            switch char {
+            case "(","{","[":
+                stack.append(char)
+            case ")","}","]":
+                
+                guard let top = stack.popLast()
+                    , counterBracket[char]! == top
+                    else {
+                        return false
+                }
+                
+            default:
+                return false
+            }
+        }
+        
+        return stack.isEmpty
     }
     
     func readInput() -> [String] {
